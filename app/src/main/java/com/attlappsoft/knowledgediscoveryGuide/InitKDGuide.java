7a538ccb7a2690b6f5b24bd9344844b6b667fb48
Efrain Jacobo Avila Juarez <ejavilaj@gmail.com>
2019-06-09 08:08:16 -0500

    Primer pantalla e iconos agregados. Colores agregados

---
+++ app/src/main/java/com/attlappsoft/knowledgediscoveryGuide/InitKDGuide.java
@@ -0,0 +1,13 @@
+package com.attlappsoft.knowledgediscoveryGuide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitKDGuide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init_kdguide);
+    }
+}
